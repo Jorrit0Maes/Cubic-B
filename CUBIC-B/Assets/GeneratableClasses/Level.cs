ff0043aa870160e6f186cb087cf83a91ec906669
Jorrit0Maes <116198426+Jorrit0Maes@users.noreply.github.com>
2023-01-16 20:06:09 +0100

    spawning boxes
    obstacle boxes spawned in automatically

--- CUBIC-B/Assets/GeneratableClasses/Level.cs
+++ CUBIC-B/Assets/GeneratableClasses/Level.cs
@@ -22,11 +22,13 @@ public class Level : MonoBehaviour
     public int minPlatformXspacing;
     public Transform squareExmp;
     private ArrayList platforms;
+    public float sizeOfBox;
 
     private void Awake()
     {
 
         generateLevelPlatforms();
+        placeBoxes();
 
     }
 
@@ -58,7 +60,7 @@ public class Level : MonoBehaviour
             Console.WriteLine("in while");
 
             float length = random.Next(minPlatformSize,maxPlatformSize);
-            length /= 10;
+            
 
 
             //rand doet allleen ints dus we delen door 100 om een getal tot 2 na de komme te krijgen dunno Y ma werkt niet als ik dat in de toewijzing doe beste gok is Int reasons
@@ -80,7 +82,6 @@ public class Level : MonoBehaviour
 
             Platform tempPlatform = new Platform(new Vector2(lastPoint.x+xSpacing, lastPoint.y + ySpacing), new Vector2(lastPoint.x +xSpacing+(length*4), lastPoint.y + ySpacing  - 3f));
             
-           //TODO: vertical
             lengthleft -= length;
             lastPoint.x = tempPlatform.endPoint.x;
             lastPoint.y = tempPlatform.startPoint.y;  
@@ -95,5 +96,62 @@ public class Level : MonoBehaviour
        
     }
 
+    private void placeBoxes()
+    {
 
+        System.Random random = new System.Random();
+
+        foreach(Platform platform in platforms) 
+        {
+
+            float platformlength = platform.endPoint.x - platform.startPoint.x;
+            int chanceofBoxSpawning = random.Next(minPlatformSize,maxPlatformSize);
+            // the larger the platform the higher the chance of a box spawning or if big enough we defintetly spawn .
+            if (platformlength >= maxPlatformSize/2 || chanceofBoxSpawning < platformlength)
+            {
+                // we ensure not to fill the platform with so much boxes that it is all box  => platformLength/2 = number of boxes *sizeof of a box only half of the length filled
+
+                int maxNumberOfboxes = (int)Math.Floor(platformlength / 2 / sizeOfBox);
+
+                random.Next(maxNumberOfboxes);
+
+                ArrayList boxes = new ArrayList();
+                for (int i = 0; i < maxNumberOfboxes; i++)
+                {
+                    //to not put it on the front edge so we can make the jump or off the platform we adjust the limits
+                    int boxVectorX = random.Next((int) Math.Ceiling(platform.startPoint.x) +1 , (int) platform.endPoint.x -1);
+                    Box boxTemplate = new Box(new Vector2(boxVectorX, platform.startPoint.y),sizeOfBox,sizeOfBox);
+
+                    if(boxes.Count == 0 || !checkIfBoxInOtherBox(boxes, boxTemplate))
+                    {
+                        boxes.Add(boxTemplate);
+                        Transform t = Instantiate(squareExmp, boxTemplate.origin,Quaternion.identity);
+                        t.localScale= new Vector3(boxTemplate.length/4,boxTemplate.heigth/4, 0);
+                        t.gameObject.GetComponent<SpriteRenderer>().color = Color.black;
+                        
+                    }
+
+                }
+
+
+            }
+          
+            
+        }
+    }
+
+    private bool checkIfBoxInOtherBox(ArrayList boxes, Box boxTemplate)
+    {
+       if(boxes.Count == 0) return false;
+
+       foreach(Box box in boxes)
+       {
+            //check of het startpunt van de box tussen het einde of begin van de al bestaande boxen valt of korter voor het begin dan de box die bijkomt lang is
+            if(box.startPoint.x< boxTemplate.startPoint.x - boxTemplate.length && boxTemplate.startPoint.x < box.endPoint.x)
+            {
+                return true;
+            }
+       }
+       return false;    
+    }
 }
--- CUBIC-B/Assets/GeneratableClasses/Objects/Box.cs
+++ CUBIC-B/Assets/GeneratableClasses/Objects/Box.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Box : Obstacle
 {
-    public Box(Vector2 startPoint, Vector2 endPoint)
+    public float length { get; set; }
+    public float heigth { get; set; }
+  
+    public Box(Vector2 startPoint, float length, float heigth)
     {
         this.startPoint = startPoint;
-        this.endPoint = endPoint;
-        /*
-		 * this.sprite =
-		 * this.collider =
-		 * */
+        this.length = length;
+        this.heigth = heigth;
+        this.endPoint = new Vector2(startPoint.x + length,  startPoint.y+ heigth);
+        this.origin = new Vector2(startPoint.x + (endPoint.x - startPoint.x) / 2, endPoint.y + (startPoint.y - endPoint.y) / 2);
+
     }
 }
